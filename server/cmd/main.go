24a27e16e6de8f48a71d550d281b8c89c659243a
Andrei Sibrinin <sibrinin.andrei@gmail.com>
2023-08-16 20:51:22 +0500

    Fixed limitation of concurrent method calls

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	v1 "github.com/AndreySibrinin/grspSendingFiles/proto/v1"
 	"github.com/AndreySibrinin/grspSendingFiles/server/internal/config"
@@ -10,39 +9,19 @@ import (
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"os"
 )
-
-const (
-	maxUploadCons   = 10
-	maxDownloadCons = 10
-	maxListCons     = 100
-)
-
-var uploadLimiter = make(chan struct{}, maxUploadCons)
-var downloadLimiter = make(chan struct{}, maxDownloadCons)
-var listLimiter = make(chan struct{}, maxListCons)
-
-func limitHandler(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-
-	switch info.FullMethod {
-
-	case "/v1.FileUploadService/UploadFile":
-		uploadLimiter <- struct{}{}
-		defer func() { <-uploadLimiter }()
-
-	case "/v1.FileUploadService/DownloadFile":
-		downloadLimiter <- struct{}{}
-		defer func() { <-downloadLimiter }()
-
-	case "/v1.FileUploadService/GetListFiles":
-		listLimiter <- struct{}{}
-		defer func() { <-listLimiter }()
-	}
-
-	return handler(ctx, req)
-}
 
 func main() {
+	
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(0)
+}
+
+func run() error {
 
 	cnf := config.Init()
 
@@ -50,19 +29,22 @@ func main() {
 
 	lis, err := net.Listen("tcp", grpcAddr)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		return fmt.Errorf("failed to listen: %v", err)
 	}
 
 	fileRepo := repo.NewFileRepo(cnf.Storage.FilesPath)
 
 	fileService := fileservices.New(fileRepo)
 
-	s := grpc.NewServer(grpc.UnaryInterceptor(limitHandler))
+	s := grpc.NewServer()
 
 	v1.RegisterFileUploadServiceServer(s, fileService)
 
 	log.Printf("server listening at %v", lis.Addr())
+
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		return fmt.Errorf("failed to serve: %v", err)
 	}
+
+	return nil
 }
--- server/internal/services/fileservices/service.go
+++ server/internal/services/fileservices/service.go
@@ -12,111 +12,147 @@ import (
 	"time"
 )
 
+const (
+	maxUploadCons   = 10
+	maxDownloadCons = 10
+	maxListCons     = 100
+)
+
 type Service struct {
 	v1.UnimplementedFileUploadServiceServer
-	repo FileRepo
+	repo       FileRepo
+	uploadCh   chan struct{}
+	downloadCh chan struct{}
+	listCh     chan struct{}
 }
 
 func New(fr FileRepo) *Service {
 	return &Service{
-		repo: fr,
+		repo:       fr,
+		uploadCh:   make(chan struct{}, maxUploadCons),   // Ограничение до 10 параллельных загрузок
+		downloadCh: make(chan struct{}, maxDownloadCons), // Ограничение до 10 параллельных скачиваний
+		listCh:     make(chan struct{}, maxListCons),     // Ограничение до 100 параллельных запросов списка
 	}
 }
-
 func (s *Service) UploadFile(stream v1.FileUploadService_UploadFileServer) error {
+	select {
+	case s.uploadCh <- struct{}{}:
 
-	var fileBytes []byte
-	var fileName string
-	for {
-		chunk, err := stream.Recv()
-		if err == io.EOF {
+		defer func() { <-s.uploadCh }()
 
-			file := &models.File{
-				Name:    fileName,
-				Content: fileBytes,
+		var fileBytes []byte
+		var fileName string
+		for {
+			chunk, err := stream.Recv()
+			if err == io.EOF {
+
+				file := &models.File{
+					Name:    fileName,
+					Content: fileBytes,
+				}
+
+				if err = s.repo.UploadFile(file); err != nil {
+					log.Printf("Failed to upload file '%s': %v", file.Name, err)
+					return status.Errorf(codes.Internal, "failed to upload file: %v", err)
+				}
+
+				log.Printf("File '%s' uploaded successfully", "test.txt")
+				return stream.SendAndClose(&v1.FileUploadResponse{
+					Message: "File uploaded successfully.",
+				})
 			}
 
-			if err = s.repo.UploadFile(file); err != nil {
-				log.Printf("Failed to upload file '%s': %v", file.Name, err)
-				return status.Errorf(codes.Internal, "failed to upload file: %v", err)
+			if err != nil {
+				return status.Errorf(codes.Internal, "error stream: %s", err)
 			}
 
-			log.Printf("File '%s' uploaded successfully", "test.txt")
-			return stream.SendAndClose(&v1.FileUploadResponse{
-				Message: "File uploaded successfully.",
-			})
+			fileBytes = append(fileBytes, chunk.GetFileChunk()...)
+			fileName = chunk.GetFileName()
 		}
 
-		if err != nil {
-			return status.Errorf(codes.Internal, "error stream: %s", err)
-		}
-
-		fileBytes = append(fileBytes, chunk.GetFileChunk()...)
-		fileName = chunk.GetFileName()
+	case <-stream.Context().Done():
+		return stream.Context().Err()
 	}
+
 }
 
 func (s *Service) DownloadFile(req *v1.FileDownloadRequest, stream v1.FileUploadService_DownloadFileServer) error {
+	select {
+	case s.downloadCh <- struct{}{}:
+		defer func() { <-s.downloadCh }()
 
-	file, err := s.repo.DownloadFile(req.GetFileName())
-	if err != nil {
-		log.Printf("Failed to download file '%s': %v", req.GetFileName(), err)
-		return status.Errorf(codes.Internal, "failed to download file: %v", err)
-	}
-	optimalChunkSize := 512 * 1024
+		file, err := s.repo.DownloadFile(req.GetFileName())
+		if err != nil {
+			log.Printf("Failed to download file '%s': %v", req.GetFileName(), err)
+			return status.Errorf(codes.Internal, "failed to download file: %v", err)
+		}
+		optimalChunkSize := 512 * 1024
 
-	for i := 0; i < len(file.Content); i += optimalChunkSize {
-		end := i + optimalChunkSize
-		if end > len(file.Content) {
-			end = len(file.Content)
+		for i := 0; i < len(file.Content); i += optimalChunkSize {
+			end := i + optimalChunkSize
+			if end > len(file.Content) {
+				end = len(file.Content)
+			}
+
+			chunk := file.Content[i:end]
+
+			if err := stream.Send(&v1.FileDownloadResponse{FileContent: chunk}); err != nil {
+				return status.Errorf(codes.Internal, "Failed to send chunk: %v", err)
+			}
 		}
 
-		chunk := file.Content[i:end]
+		log.Printf("File '%s' downloaded successfully", file.Name)
+		return nil
 
-		if err := stream.Send(&v1.FileDownloadResponse{FileContent: chunk}); err != nil {
-			return status.Errorf(codes.Internal, "Failed to send chunk: %v", err)
-		}
+	case <-stream.Context().Done():
+		return stream.Context().Err()
 	}
 
-	log.Printf("File '%s' downloaded successfully", file.Name)
-	return nil
 }
 
 func (s *Service) GetListFiles(req *v1.ListFilesRequest, stream v1.FileUploadService_GetListFilesServer) error {
-	files, err := s.repo.GetListFiles()
-	if err != nil {
-		log.Printf("Failed to get file list: %v", err)
-		return status.Errorf(codes.Internal, "Failed to get file list: %v", err)
-	}
+	select {
+	case s.listCh <- struct{}{}:
+		defer func() { <-s.listCh }()
 
-	for _, file := range files {
-		t, err := times.Stat(file)
+		files, err := s.repo.GetListFiles()
 		if err != nil {
-			log.Printf("Failed to get file times for '%s': %v", file, err)
-			return status.Errorf(codes.Internal, "Failed to get file times for '%s': %v", file, err)
+			log.Printf("Failed to get file list: %v", err)
+			return status.Errorf(codes.Internal, "Failed to get file list: %v", err)
 		}
 
-		var modTime, createTime time.Time
-		modTime = t.ModTime()
-		createTime = t.ModTime()
+		for _, file := range files {
+			t, err := times.Stat(file)
+			if err != nil {
+				log.Printf("Failed to get file times for '%s': %v", file, err)
+				return status.Errorf(codes.Internal, "Failed to get file times for '%s': %v", file, err)
+			}
 
-		// In some Unix-like systems, it is not possible to directly obtain the file creation date.
-		if t.HasBirthTime() {
-			createTime = t.BirthTime()
+			var modTime, createTime time.Time
+			modTime = t.ModTime()
+			createTime = t.ModTime()
+
+			// In some Unix-like systems, it is not possible to directly obtain the file creation date.
+			if t.HasBirthTime() {
+				createTime = t.BirthTime()
+			}
+
+			res := &v1.ListFilesResponse{
+				FileName:   filepath.Base(file),
+				DateCreate: modTime.Format("02.01.2006 15:04:05"),
+				DateChange: createTime.Format("02.01.2006 15:04:05"),
+			}
+
+			if err := stream.Send(res); err != nil {
+				log.Printf("Failed to send file list for '%s': %v", file, err)
+				return status.Errorf(codes.Internal, "Failed to send file list for '%s': %v", file, err)
+			}
 		}
 
-		res := &v1.ListFilesResponse{
-			FileName:   filepath.Base(file),
-			DateCreate: modTime.Format("02.01.2006 15:04:05"),
-			DateChange: createTime.Format("02.01.2006 15:04:05"),
-		}
-
-		if err := stream.Send(res); err != nil {
-			log.Printf("Failed to send file list for '%s': %v", file, err)
-			return status.Errorf(codes.Internal, "Failed to send file list for '%s': %v", file, err)
-		}
+		log.Printf("File list sent successfully")
+		return nil
+	case <-stream.Context().Done():
+		return stream.Context().Err()
 	}
 
-	log.Printf("File list sent successfully")
-	return nil
 }
